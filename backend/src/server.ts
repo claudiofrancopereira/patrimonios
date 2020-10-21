0a5fa4568412c9940ba05aac706a2932b60fc1f4
Claudio Pereira <claudiofrancopereira@gmail.com>
2020-10-21 04:45:02 -0300

    Environment Vars

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,16 @@
+import 'dotenv/config';
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 import './database/connection';
 
 import routes from './routes'
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
