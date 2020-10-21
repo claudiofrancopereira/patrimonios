99ec9c2f2bb46d45a22e2a0970c0e4c206b9e2dd
Claudio Pereira <claudiofrancopereira@gmail.com>
2020-10-21 01:34:59 -0300

    Added images table

---
+++ backend/src/database/migrations/1603253094984-create_images.ts
@@ -0,0 +1,45 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createImages1603253094984 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'images',
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          unsigned: true,
+          isPrimary: true,
+          isGenerated: true,
+          generationStrategy: 'increment',
+        },
+
+        {
+          name: 'path',
+          type: 'varchar',
+        },
+
+        {
+          name: 'patrimonio_id',
+          type: 'integer'
+        }
+      ],
+
+      foreignKeys: [
+        {
+          name: 'PatrimonioImage',
+          columnNames: ['patrimonio_id'],
+          referencedTableName: 'patrimonios',
+          referencedColumnNames: ['id'],
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        }
+      ]
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+  }
+
+}
